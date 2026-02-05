d4da24c030db6e79e6aabe53896b87759e8b8d80
WillianKowalski <williansouzakowalski@gmail.com>
2026-02-05 14:52:46 +0000

    Finalizado Trabalho DesafioTema2

--- DesafioTema2.c
+++ DesafioTema2.c
@@ -1,1 +1,72 @@
+#include <stdio.h>
+
+int main() {
+
+    /* ===== CARTA 1 ===== */
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontos1;
+
+    /* ===== CARTA 2 ===== */
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    int populacao2;
+    float area2;
+    float pib2;
+    int pontos2;
+
+    /* Leitura da Carta 1 */
+    printf("Carta 1\n");
+    printf("Estado (A a H): ");
+    scanf(" %c", &estado1);
+
+    printf("Codigo da carta (ex: A01): ");
+    scanf("%s", codigo1);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", cidade1);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Area (km²): ");
+    scanf("%f", &area1);
+
+    printf("PIB (bilhoes de reais): ");
+    scanf("%f", &pib1);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos1);
+
+    /* Leitura da Carta 2 */
+    printf("\nCarta 2\n");
+    printf("Estado (A a H): ");
+    scanf(" %c", &estado2);
+
+    printf("Codigo da carta (ex: B02): ");
+    scanf("%s", codigo2);
+
+    printf("Nome da cidade: ");
+    scanf(" %[^\n]", cidade2);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("Area (km²): ");
+    scanf("%f", &area2);
+
+    printf("PIB (bilhoes de reais): ");
+    scanf("%f", &pib2);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontos2);
+
+
+    return 0;
+}
 
